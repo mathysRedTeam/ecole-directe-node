574f651ee2953a11f5707478a5e7d5d5b7eb4486
Mathys <mathysdevnode@gmail.com>
2022-03-20 19:30:10 +0100

    change log

--- test.js
+++ test.js
@@ -6,15 +6,17 @@ const myED=require("./");
 (async () => {
     //retourne les informations de la session et de l'élève
     let session=await myED.login(process.env.EDUsername,process.env.EDPassword);
-    console.log(session.token)
+    let token=session.token;
+    console.log(token);
+
 
     //retourne la liste des messages
-    let myMessages=await myED.getMessages(session);
+    let myMessages=await myED.getAllMessages(session);
 
 
     //retourne le dernier message reçu
     let firstMessageId=myMessages.data.messages.received[0].id;
-    let firstMessageContent=await myED.getMessageContent(session,firstMessageId);
+    let firstMessageContent=await myED.getMessage(session,firstMessageId);
     console.log(firstMessageContent)
 
     //retourne l'emploi du temps
